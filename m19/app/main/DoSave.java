5a61c180255e7bb414e2c5d8d99b2df64a44a949
Nuno <nfnvsc@hotmail.com>
2019-11-16 15:46:11 +0000

    Fixed comments

--- m19/app/main/DoSave.java
+++ m19/app/main/DoSave.java
@@ -5,20 +5,13 @@ import java.io.IOException;
 import m19.core.LibraryManager;
 import m19.core.exception.MissingFileAssociationException;
 import pt.tecnico.po.ui.Command;
-import pt.tecnico.po.ui.Display;
 import pt.tecnico.po.ui.Input;
-
-import java.util.Scanner;
-
-// FIXME import other core concepts
-// FIXME import other ui concepts
 
 /**
  * 4.1.1. Save to file under current name (if unnamed, query for name).
  */
 public class DoSave extends Command<LibraryManager> {
   
-  // FIXME define input fields
   private Input<String> _filename;
 
   /**
@@ -26,7 +19,6 @@ public class DoSave extends Command<LibraryManager> {
    */
   public DoSave(LibraryManager receiver) {
     super(Label.SAVE, receiver);
-    // FIXME initialize input fields
     _filename = _form.addStringInput(Message.newSaveAs());
   }
 
