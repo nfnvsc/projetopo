f050c2666f31cf29851b736cd02fffc5220c3abd
Miguel Silva <miguel04silva18@gmail.com>
2019-11-16 16:14:12 +0000

    Added javadoc

--- m19/core/Library.java
+++ m19/core/Library.java
@@ -23,13 +23,40 @@ public class Library implements Serializable {
   private static final long serialVersionUID = 201901101348L;
 
   // FIXME define attributes
+  /**
+   * The Id to be assigned to the next Work
+   */
   private int _nextWorkId;
+
+  /**
+   * The Id to be assigned to the next User
+   */
   private int _nextUserId;
+
+  /**
+   * The current date
+   * @see Date
+   */
   private Date _date;
+
+  /**
+   * A map of all the users in the Library (indexed by UserId)
+   * 
+   * @see User#getId()
+   */
   private Map<Integer, User> _users = new TreeMap<>();
+
+  /**
+   * A map of all the works in the Library (indexed by WorkId)
+   * 
+   * @see Work#getId()
+   */
   private Map<Integer, Work> _works = new HashMap<>();
 
   // FIXME define contructor(s)
+  /**
+   * Makes a {@code Library} instace 
+   */
   public Library() {
     _nextUserId = 0;
     _nextWorkId = 0;
@@ -39,6 +66,14 @@ public class Library implements Serializable {
   // FIXME define methods
 
   //Metodos User
+  /**
+   * Add User to the Library TreeMap
+   * 
+   * @param user  the user that is meant to be add to the Library database
+   * @return      the user identification in the database
+   * @see         User
+   * 
+   */ 
   public int addUser(User user) {
     user.setUserId(_nextUserId);
     _users.put(user.getId(), user);
@@ -46,32 +81,71 @@ public class Library implements Serializable {
     return user.getId();
   }
 
+  /**
+   * Add Work to the Library TreeMap
+   * 
+   * @param Work  the uwork that is meant to be add to the Library database
+   * @see         Work
+   * 
+   */
   public void addWork(Work work) {
     work.setWorkId(_nextWorkId);
     _works.put(work.getId(), work);
     _nextWorkId++;
   }
   
+  /**
+   * Gets a user of associated to the given User Id
+   * 
+   * @param id    the id of a User 
+   * @return      the user associated to that id
+   */
   public User getUser(int id) {
     return _users.get(id);
   }
 
+  /**
+   * Gets a map of all the users
+   * 
+   * @return      a Map of all the users
+   */
   protected Map<Integer, User> getAllUsers(){
     return _users;
   }
 
+  /**
+   * Gets current date
+   * 
+   * @return      Current date
+   */
   public int getDate(){
     return _date.getCurrentDate();
   }
 
+  /**
+   * Adds to the current date the number of days to advance
+   * 
+   * @param nDays Number of days to advance the current date
+   */
   public void advanceDate(int nDays) {
     _date.advanceDays(nDays);
   }
 
+  /**
+   * Gets a Work of associated to the given Work Id
+   * 
+   * @param id    the id of a Work
+   * @return      the Work associated to that Id
+   */
   public Work getWork(int id){
     return _works.get(id);
   }
 
+  /**
+   * Gets the number of different Works in the library
+   * 
+   * @return      the number of Works
+   */
   public int getNumberWorks(){
     return _nextWorkId - 1;
   }
