a29cd096f54c9775c13c9b29f19f64bba3139d70
Miguel Silva <miguel04silva18@gmail.com>
2019-12-04 15:59:55 +0000

    Minor fixes

--- m19/core/Users/Behavior.java
+++ m19/core/Users/Behavior.java
@@ -2,4 +2,8 @@ package m19.core.Users;
 
 public interface Behavior {
     public void doBehavior(User user);
+    public String toString();
+    public int getDeadline(int AvailableCopies);
+    public int getMaxRequestPrice();
+    public int getMaxRequests();
 }
