fb28de7d2b41dc0759fb5d667267242749f33cd8
Miguel Silva <miguel04silva18@gmail.com>
2019-12-04 14:38:25 +0000

    Added Requests.java

---
+++ m19/core/Requests.java
@@ -0,0 +1,33 @@
+package m19.core;
+
+import m19.core.Works.*;
+
+/*
+ * Mediator class between Users and Works
+ */
+public class Requests {
+    
+    private int _deadline; 
+    private User _user;
+    private Work _work;
+
+    public Requests(User user, Work work) {
+        _user = user;
+        _work = work;
+    }
+    public void setDeadline(int deadline) {
+        _deadline = deadline;
+    }
+    public void updateDeadline(int daysAdvanced) {
+        _deadline -= daysAdvanced;
+    }
+    public User getUser() {
+        return _user;
+    }
+    public Work getWork() {
+        return _work;
+    }
+    public int getDeadline() {
+        return _deadline;
+    }
+}
