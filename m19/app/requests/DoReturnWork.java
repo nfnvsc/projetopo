00f7d4094c3f03b4ddb4b9691a4e7e9d28016387
Nuno <nfnvsc@hotmail.com>
2019-12-08 22:47:18 +0000

    Minor fix

--- m19/app/requests/DoReturnWork.java
+++ m19/app/requests/DoReturnWork.java
@@ -2,6 +2,7 @@ package m19.app.requests;
 
 import pt.tecnico.po.ui.Input;
 import m19.app.exception.NoSuchUserException;
+import m19.app.exception.NoSuchWorkException;
 import m19.app.exception.WorkNotBorrowedByUserException;
 import m19.core.LibraryManager;
 import m19.core.exception.BadEntrySpecificationException;
@@ -38,24 +39,22 @@ public class DoReturnWork extends Command<LibraryManager> {
       value = _receiver.returnWork(_userID.value(), _workID.value());
     } catch (WorkNotBorrowedByUserException wnkbbuse) {
         throw new WorkNotBorrowedByUserException(_workID.value(), _userID.value());
-    }
+    } catch (NoSuchUserException nsue) {
+        throw new NoSuchUserException(_userID.value());
+    } catch (NoSuchWorkException nswe) {
+        throw new NoSuchWorkException(_workID.value());
+  }
     
     if (value != 0) {
       _display.popup(Message.showFine(_userID.value(), value));
-
+      
       _form.clear();
       _decision = _form.addStringInput(Message.requestFinePaymentChoice());
       _form.parse();
 
-      if (_decision.value().equals("s")) {
-        try {
+      if (_decision.value().equals("s")) 
           _receiver.payFine(_userID.value());
-        } catch (NoSuchUserException e) {
-          throw new NoSuchUserException(_userID.value());
-        }
-      } else {
-        _receiver.addFine(_userID.value(), value);
-      }
+      
     }
 
   }
